5fd5b4e752e2ecbc5a2d11fb2d19d5246132dc9b
gumburtu <aslanseval1986@gmail.com>
2025-06-10 10:56:08 +0100

    RestAssuredV16

--- src/test/java/baseurl/PetStoreBaseUrl.java
+++ src/test/java/baseurl/PetStoreBaseUrl.java
@@ -11,7 +11,6 @@ public class PetStoreBaseUrl {
     Her test ten önce calisarak ihtiyac duyabileceğimiz cesitli configurasyonlari tamamlamaktir
     Mesela Baseurl, content type, authorization gibi yapilandirmalari tek bir merkezden güncellemeyi saglar
     böylece testlerin bakimi daha kolay olur
-
      */
     protected RequestSpecification spec;
 
